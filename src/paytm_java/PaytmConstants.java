f37f379275677eb605095038f32e612f82f119b2
Diksha2008 <dikshamanchanda20@gmail.com>
2018-07-16 20:20:57 +0530

    add paytm files

---
+++ src/paytm_java/PaytmConstants.java
@@ -0,0 +1,10 @@
+package paytm_java;
+public class PaytmConstants {
+  public final static String MID="Notess00475747241101";
+  public final static String MERCHANT_KEY="Uv1iA%lMWOcoHjx@";
+  public final static String INDUSTRY_TYPE_ID="Retail";
+  public final static String CHANNEL_ID="WEB";
+  public final static String WEBSITE="WEBSTAGING";
+  public final static String PAYTM_URL="https://securegw-stage.paytm.in/theia/processTransaction"; 
+  
+}
